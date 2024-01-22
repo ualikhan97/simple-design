3a352d549a940f68657e07b12689652f3fd658a3
ualikhan97 <supercilium97@gmail.com>
2024-01-22 18:15:27 +0600

    first

---
+++ js/sript.js
@@ -0,0 +1,7 @@
+let burgerElement = document.querySelector(".burger");
+
+burgerElement.addEventListener("click", function () {
+   this.classList.toggle("active");
+   document.querySelector(".header__shell").classList.toggle("active");
+   document.body.classList.toggle("lock");
+});
